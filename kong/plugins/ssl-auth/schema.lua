ae10ca0feef07a435a66ac05072b852d79464752
swifthorseman <swifthorseman@protonmail.com>
2017-07-19 16:46:50 +0100

    Define the schema for the plug-in's configuration

--- kong/plugins/ssl-auth/schema.lua
+++ kong/plugins/ssl-auth/schema.lua
@@ -1,11 +1,4 @@
 return {
-  no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
-  fields = {
-    -- Describe your plugin's configuration's schema here.
-    
-  },
-  self_check = function(schema, plugin_t, dao, is_updating)
-    -- perform any custom verification
-    return true
-  end
+  no_consumer = true, -- the plug-in is api-wide; not per consumer.
+  fields = {} -- needs to be explicit; otherwise, it doesn't work.
 }
